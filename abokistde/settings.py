33761b63a2559934ebb55f7938aa9b8803a60090
nilshellerhoff <nils.hellerhoff@gmail.com>
2023-12-18 18:33:16 +0100

    remove livereload

--- abokistde/settings.py
+++ abokistde/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'livereload.middleware.LiveReloadScript',
+    # 'livereload.middleware.LiveReloadScript',
 ]
 
 ROOT_URLCONF = 'abokistde.urls'
