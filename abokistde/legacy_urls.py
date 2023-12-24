e37a00b7c0a948df15a0c1628191c88096f4e312
nilshellerhoff <nils.hellerhoff@gmail.com>
2023-12-24 17:27:37 +0100

    fix imports

--- abokistde/legacy_urls.py
+++ abokistde/legacy_urls.py
@@ -1,17 +1,17 @@
 from django.urls import path
-from . import legacy_views as views
+from abokistde.legacy_views import videos, fetch_youtube, user_login, user_logout, user_checktoken, user_add, \
+    insert_channel, insert_channel_by_id, delete_channel, search_online
 
 urlpatterns = [
     # Legacy endpoints for old frontend
-    path('api/videos', views.videos, name="videos"),
-    path('fetch_youtube', views.fetch_youtube, name='fetch_youtube'),
-    path('user/login', views.user_login, name='user_login'),
-    path('user/logout', views.user_logout, name='user_logout'),
-    path('user/check_token', views.user_checktoken, name='user_checktoken'),
-    path('user/add', views.user_add, name='user_add'),
-    path('api/insert_channel', views.insert_channel, name='insert_channel'),
-    path('api/insert_channel_by_id', views.insert_channel_by_id, name='insert_channel_by_id'),
-    path('api/delete_channel', views.delete_channel, name='delete_channel'),
-    path('search', views.search, name='search'),
-    path('search_online', views.search_online, name='search_online'),
+    path('api/videos', videos, name="videos"),
+    path('fetch_youtube', fetch_youtube, name='fetch_youtube'),
+    path('user/login', user_login, name='user_login'),
+    path('user/logout', user_logout, name='user_logout'),
+    path('user/check_token', user_checktoken, name='user_checktoken'),
+    path('user/add', user_add, name='user_add'),
+    path('api/insert_channel', insert_channel, name='insert_channel'),
+    path('api/insert_channel_by_id', insert_channel_by_id, name='insert_channel_by_id'),
+    path('api/delete_channel', delete_channel, name='delete_channel'),
+    path('search_online', search_online, name='search_online'),
 ]
